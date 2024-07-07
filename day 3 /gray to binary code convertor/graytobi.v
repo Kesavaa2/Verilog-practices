32273d2b5070ad6b0ec4df8e68ab17f0058b3231
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-07 14:26:00 +0530

    Create graytobi.v

---
+++ day 3 /gray to binary code convertor/graytobi.v
@@ -0,0 +1,10 @@
+module graytobi (
+input [3:0] G,
+output [3:0]B
+); 
+
+assign B[0] = G[3] ^ G[2] ^ G[1] ^ G[0];
+assign B[1] = G[3] ^ G[2] ^ G[1];
+assign B[2] = G[3] ^ G[2];
+assign B[3] = G[3];
+endmodule 
