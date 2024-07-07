c89268a5a7c1894bc6b61483f2f6e09a12ec62ea
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-07 14:39:48 +0530

    Create comparator_tb.v

---
+++ day 3 /comparator/comparator_tb.v
@@ -0,0 +1,14 @@
+module comparator_tb;
+reg [3:0] A, B; 
+wire A_grt_B, A_less_B, A_eq_B;
+comparator comp(A, B, A_grt_B, A_less_B, A_eq_B);
+initial 
+begin
+ $monitor("A = %0h, B = %0h -> A_grt_B = %0b, A_less_B = %0b, A_eq_B = %0b", A, B, A_grt_B, A_less_B, A_eq_B);
+repeat(5) begin
+A=$random;
+B=$random;
+#10;
+end 
+end 
+endmodule
