064a67b652c499aae63245c8d5acb8f1ce0ce0ba
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-10 22:58:56 +0530

    Create odd_number.v

---
+++ day 6/odd number using task/odd_number.v
@@ -0,0 +1,15 @@
+module odd_number;
+  
+  task odd(input int a);
+    begin
+      if(a%2!=0)
+        $display(a," is a odd number");
+      else
+        $display(a," is not a odd number");
+    end
+  endtask
+  
+  initial begin
+    odd(5);
+  end
+endmodule
