13c3893355308991b7e363e56a2f67553e3aa4e1
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-10 22:28:03 +0530

    Update sr_flip_flop.v

--- day 6/SR Flip Flop/sr_flip_flop.v
+++ day 6/SR Flip Flop/sr_flip_flop.v
@@ -14,7 +14,7 @@ module sr_flip_flop (
                 2'b00: q <= q;       
                 2'b01: q <= 1'b0;     
                 2'b10: q <= 1'b1;    
-                2'b11: q <= ~q;      
+                2'b11: q <= 1'bx;      
                 default: q <= q;       
             endcase
         end
