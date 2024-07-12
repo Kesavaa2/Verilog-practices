3ce70ccf8a2b9fafb82d303041be826cc7fcfcf2
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-12 15:37:06 +0530

    Create down_counter.v

---
+++ day 8 /down counter/down_counter.v
@@ -0,0 +1,15 @@
+module down_counter(input [3:0]a,
+                  input rst,clk,
+                  output reg [3:0]y);
+  
+  always @(posedge clk)begin
+    
+    if (rst) 
+      y<=4'b0000;
+    
+    else  
+        y<=a-1;
+      
+   
+  end
+endmodule
