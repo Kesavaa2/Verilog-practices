af3f9751713837aa86e4a32a9596d374c20f0d68
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-16 22:14:12 +0530

    Create bcd_to_seven_seg_tb.v

---
+++ day 11/bcd to 7 segment display using behavioural/bcd_to_seven_seg_tb.v
@@ -0,0 +1,35 @@
+module  bcd_to_seven_seg_tb;
+
+     reg [3:0] bcd_input;
+    wire [6:0] seg_output;
+
+    bcd_to_seven_seg dut(
+        .bcd(bcd_input),
+        .seg_out(seg_output)
+    );
+
+    reg clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        bcd_input = 4'b0000;
+        
+        #10 bcd_input = 4'b0001;
+        #10 bcd_input = 4'b0010;
+        #10 bcd_input = 4'b0011;
+        #10 bcd_input = 4'b0100;
+        #10 bcd_input = 4'b0101;
+        #10 bcd_input = 4'b0110;
+        #10 bcd_input = 4'b0111;
+        #10 bcd_input = 4'b1000;
+        #10 bcd_input = 4'b1001;
+        #10 bcd_input = 4'b1010;
+
+        #10 $finish;
+    end
+  
+  initial begin
+    $monitor("%t | bcd_input=%b | seg_output=%b",$time,bcd_input,seg_output);
+  end
+
+endmodule
