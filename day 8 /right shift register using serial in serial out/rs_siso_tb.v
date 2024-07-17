0af0ce3c02f6d1775301e17dce4be19f0f6123c7
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-17 20:33:29 +0530

    Update rs_siso_tb.v

--- day 8 /right shift register using serial in serial out/rs_siso_tb.v
+++ day 8 /right shift register using serial in serial out/rs_siso_tb.v
@@ -1,16 +1,16 @@
 module rs_siso_tb;
 reg clk,rst;
 reg s_in;
-wire p_out;
+wire s_out;
 
-rs_siso SISO(clk,rst,s_in,p_out);
+  rs_siso SISO(clk,rst,s_in,s_out);
 
 always #5 clk=~clk;
 initial begin
 clk=0;rst=1; s_in=1;
 #20 rst=0; s_in=1;#30;
 #150 $stop;
-$monitor("clk=%b,rst=%b,s_in=%b,p_out=%b",$time,clk,rst,s_in,p_out);
+  $monitor("clk=%b,rst=%b,s_in=%b,s_out=%b",$time,clk,rst,s_in,s_out);
 $finish;
 end
 endmodule
