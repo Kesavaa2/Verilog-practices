2c357d3323139d184854c54b31a2acdd35529e1e
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-17 20:32:48 +0530

    Update rs_siso.v

--- day 8 /right shift register using serial in serial out/rs_siso.v
+++ day 8 /right shift register using serial in serial out/rs_siso.v
@@ -1,7 +1,7 @@
 module rs_siso(
 input clk,rst,
 input s_in,
-output reg p_out
+output reg s_out
 );
 reg [2:0]shift_reg;
 
@@ -9,11 +9,11 @@ always@(posedge clk or posedge rst)
 begin
 if (rst) begin
  shift_reg <= 3'b000;
- p_out <= 0;
+ s_out <= 0;
 end
 else begin
  shift_reg <= {s_in,shift_reg[2:1]};
- p_out <= shift_reg;
+ s_out <= shift_reg[0];
 end
 end
 endmodule
