f58f37aacdb10fd5af06a4d01e3069c5a5f5b6b3
Kesavaa2 <148443860+Kesavaa2@users.noreply.github.com>
2024-07-18 16:06:41 +0530

    Create ovmealy_tb.v

---
+++ day 12/mealy sequence detector overlapping/ovmealy_tb.v
@@ -0,0 +1,28 @@
+module ovmealy_tb;
+reg clk,rst,a; 
+wire out;
+
+parameter delay = 1;
+
+ovmealy_1010 OMLY(clk,rst,a,out);
+always #4 clk=~clk;
+initial begin
+clk=0;rst=0; a=1;
+#2 rst=1;
+
+#2 a=1;
+#2 a=0;
+#2 a=1;
+#2 a=0;
+#2 a=1;
+#2 a=1;
+#2 a=0;
+#2 a=1;
+#2 a=0;
+#2 a=1;
+#2 a=0;
+#2 a=1;
+
+$finish;
+end
+endmodule
